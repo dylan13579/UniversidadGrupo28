6ef7af420bb94e0aa23c3c76c5e44307e5bdfb2e
dylan13579 <jofrevillegasdylan@gmail.com>
2023-09-08 10:39:49 -0300

    cambie algo

---
+++ src/universidadgrupo28/AccesoADatos/Conexion.java
@@ -0,0 +1,41 @@
+
+package universidadgrupo28.AccesoADatos;
+
+import java.sql.*;
+
+import javax.swing.JOptionPane;
+
+
+public class Conexion {
+    
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String BD="universidad";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection connection;
+    
+    private Conexion () {}
+    
+    public static Connection getConnection(){
+        
+        if(connection==null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                
+                connection = DriverManager.getConnection(URL+BD,USUARIO,PASSWORD);
+                
+                JOptionPane.showMessageDialog(null, "Conectado");
+               
+                
+                
+            } catch (ClassNotFoundException e) {
+                JOptionPane.showMessageDialog(null, "Error al cargar DRiVER ");
+            } catch (SQLException q) {
+                JOptionPane.showMessageDialog(null, "Eror no se pudo conectar al DRIVER");
+            }
+        }
+            return connection;
+    }
+  
+    
+}
